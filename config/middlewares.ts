c2d8147dbf4db1721ed71a2da60709b3c5a0ef90
SojibHasan <sojibsrd85@gmail.com>
2025-02-20 15:33:15 +0600

    env upadated

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -23,3 +23,41 @@ export default [
   "strapi::favicon",
   "strapi::public",
 ];
+
+// module.exports = ({ env }) => [
+//   "strapi::errors",
+//   {
+//     name: "strapi::security",
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           "connect-src": ["'self'", "https:"],
+//           "default-src": ["'self'"],
+//           "img-src": [
+//             "'self'",
+//             "data:",
+//             "blob:",
+//             "market-assets.strapi.io",
+//             env("SUPABASE_URL"),
+//           ],
+//           "media-src": [
+//             "'self'",
+//             "data:",
+//             "blob:",
+//             "market-assets.strapi.io",
+//             env("SUPABASE_URL"),
+//           ],
+//         },
+//       },
+//     },
+//   },
+//   "strapi::cors",
+//   "strapi::poweredBy",
+//   "strapi::logger",
+//   "strapi::query",
+//   "strapi::body",
+//   "strapi::session",
+//   "strapi::favicon",
+//   "strapi::public",
+// ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -16,3 +16,23 @@ module.exports = ({ env }) => ({
   },
   // ...
 });
+
+// module.exports = ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: "strapi-provider-upload-supabase",
+//       providerOptions: {
+//         apiUrl: env("SUPABASE_API_URL"),
+//         apiKey: env("SUPABASE_API_KEY"),
+//         bucket: env("SUPABASE_BUCKET"),
+//         directory: env("SUPABASE_DIRECTORY"),
+//         options: {},
+//       },
+//       actionOptions: {
+//         upload: {},
+//         uploadStream: {},
+//         delete: {},
+//       },
+//     },
+//   },
+// });
