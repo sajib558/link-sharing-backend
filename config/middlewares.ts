db497d75bf33ecca58ba62ff251796860ef5fcdb
SojibHasan <sojibsrd85@gmail.com>
2025-02-22 05:36:32 +0600

    cloudinary remove

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,20 +1,7 @@
 export default [
   "strapi::logger",
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
@@ -24,7 +11,10 @@ export default [
   "strapi::public",
 ];
 
-// module.exports = ({ env }) => [
+// This is for cloudinary add
+
+// export default [
+//   "strapi::logger",
 //   "strapi::errors",
 //   {
 //     name: "strapi::security",
@@ -33,28 +23,15 @@ export default [
 //         useDefaults: true,
 //         directives: {
 //           "connect-src": ["'self'", "https:"],
-//           "default-src": ["'self'"],
-//           "img-src": [
-//             "'self'",
-//             "data:",
-//             "blob:",
-//             "market-assets.strapi.io",
-//             env("SUPABASE_URL"),
-//           ],
-//           "media-src": [
-//             "'self'",
-//             "data:",
-//             "blob:",
-//             "market-assets.strapi.io",
-//             env("SUPABASE_URL"),
-//           ],
+//           "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+//           "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+//           upgradeInsecureRequests: null,
 //         },
 //       },
 //     },
 //   },
 //   "strapi::cors",
 //   "strapi::poweredBy",
-//   "strapi::logger",
 //   "strapi::query",
 //   "strapi::body",
 //   "strapi::session",
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,38 +1,21 @@
-module.exports = ({ env }) => ({
-  // ...
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
-  },
-  // ...
-});
+export default () => ({});
 
+//  This is for cloudinary
 // module.exports = ({ env }) => ({
+//   // ...
 //   upload: {
 //     config: {
-//       provider: "strapi-provider-upload-supabase",
+//       provider: "cloudinary",
 //       providerOptions: {
-//         apiUrl: env("SUPABASE_API_URL"),
-//         apiKey: env("SUPABASE_API_KEY"),
-//         bucket: env("SUPABASE_BUCKET"),
-//         directory: env("SUPABASE_DIRECTORY"),
-//         options: {},
+//         cloud_name: env("CLOUDINARY_NAME"),
+//         api_key: env("CLOUDINARY_KEY"),
+//         api_secret: env("CLOUDINARY_SECRET"),
 //       },
 //       actionOptions: {
 //         upload: {},
-//         uploadStream: {},
 //         delete: {},
 //       },
 //     },
 //   },
+//   // ...
 // });
